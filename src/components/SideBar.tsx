6c0085055d4d435d8096d6797ab8bff5dad8d162
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-04 21:38:02 +0200

    🎨 M- refactor compoent sidebar and types

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,25 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+import { SidebarProps } from '../@types/types';
+
+export function SideBar({genres,selectedGenreId, handleClickButton}:SidebarProps) {
+  // Complete aqui   <nav className="sidebar">
+  return(
+    <>
+      <nav>
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
