47a1373aacb5596fc91f9a8e6ebac3b66f598759
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-04 22:46:37 +0200

    🎨 M- refactor code for component content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,21 @@
-export function Content() {
+import { ContentProps } from '../@types/types';
+import { MovieCard } from './MovieCard';
+import {Header} from './Header';
+import '../styles/content.scss';
+
+
+export function Content({movies, selectedGenre}:ContentProps) {
   // Complete aqui
-  
+  return(
+    <div className="container">
+      <Header selectedGenre={selectedGenre}/>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>    
+  )
 }
