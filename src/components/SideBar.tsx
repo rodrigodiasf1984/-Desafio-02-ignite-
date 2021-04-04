f83ef8ec06a83bd277202cbd3d4a2d74601b8fb5
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-04-04 22:46:57 +0200

    🎨 M- refactor code for component sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,12 @@
 import { Button } from './Button';
-import '../styles/sidebar.scss';
 import { SidebarProps } from '../@types/types';
+import '../styles/sidebar.scss';
 
 export function SideBar({genres,selectedGenreId, handleClickButton}:SidebarProps) {
   // Complete aqui   <nav className="sidebar">
   return(
     <>
-      <nav>
+      <nav className="sidebar">
         <span>Watch<p>Me</p></span>
         <div className="buttons-container">
           {genres.map(genre => (
@@ -19,6 +19,7 @@ export function SideBar({genres,selectedGenreId, handleClickButton}:SidebarProps
             />
           ))}
         </div>
+
       </nav>
     </>
   );
